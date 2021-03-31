2b3ba55d05e2629aa78cce47950f668eb3b38c14
flymonkey <2541112880@qq.com>
2021-03-31 15:03:23 +0800

    merge

---
+++ src/main/java/com/flymonkey/fileserver/mo/SysUser.java
@@ -0,0 +1,30 @@
+package com.flymonkey.fileserver.mo;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: flymonkey
+ * Date: 2021/3/31
+ * Time: 14:31
+ * Description:
+ */
+public class SysUser {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
